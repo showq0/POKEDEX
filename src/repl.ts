f57b523d6bd057a8534da4fd2128e4ecdb3cb9a0
showq0 <showqbalawneh@gmail.com>
2026-07-11 00:42:00 +0300

    REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,26 @@
-export function cleanInput(input: string): string[] {
-    return input.split(" ");
+export function cleanInput(input: string) {
+    let words = input.split(" ");
+    const result = words.map((element) => element.toLowerCase());
+    return result;
+}
+
+import { stdin, stdout } from "node:process";
+import { createInterface } from "node:readline";
+
+export const r1 = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex > ",
+});
+export function startREPL() {
+    r1.prompt();
+    r1.on("line", (input) => {
+
+        if (input != "") {
+            // console.log(`your entered: ${input}`);
+            let cleaned_output = cleanInput(input);
+            console.log(`Your command was: ${cleaned_output[0]}`);
+        }
+        r1.prompt();
+    });
 }
